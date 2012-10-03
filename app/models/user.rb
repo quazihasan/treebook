78ba7b2a8a916a7f3d070da468087911afd12524
Quazi Hasan <sunnyhasan@gmail.com>
2012-10-03 00:34:10 -0500

    update frontend for the new authentication system
    Added simple_form
    Updated several forms to boostrap/simple_form
    Added login/logout/registration/profile links to our nav
    Added a collection select to our status form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me,
   					:first_name, :last_name, :profile_name
 
+  has_many :statuses
+  
+  def full_name
+	first_name+" "+last_name
+  end
+
 end
